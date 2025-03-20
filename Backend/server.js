f032c423b8cd38575321ce9e0bd178b87ce3df7b
Sumitbose05 <bosesomen062@gmail.com>
2025-03-20 13:55:18 +0530

    CORS fix attempt-1

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 // CORS Configuration
 const corsOptions = {
     // origin: ["https://rankcheck.netlify.app", "http://localhost:5173"],
-    origin : "*",
+    origin : "https://rankcheck.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
@@ -22,16 +22,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Manually set CORS headers for all responses
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://rankcheck.netlify.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, HEAD");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://rankcheck.netlify.app");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, HEAD");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
 
 // Enable CORS preflight requests
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 // Connect to Database
 const dbConnect = require("./config/db");
