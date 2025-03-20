3c8af8e5e18ea7240e6bbdc0fca248e6038520e9
Sumitbose05 <bosesomen062@gmail.com>
2025-03-21 01:00:31 +0530

    res.cookie set to normal

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -265,9 +265,7 @@ exports.signup = async (req, res) => {
 exports.logout = (req, res) => {
     try {
         // Clear the JWT token cookie by setting its expiration to a past date
-        // res.clearCookie('newCookie');  // 'newCookie' is the name of the cookie storing the token
-
-        res.clearCookie('newCookie', {path: '/'});
+        res.clearCookie('newCookie');  // 'newCookie' is the name of the cookie storing the token
 
         console.log("req.cookie after clearing : ", req.cookies);
 
