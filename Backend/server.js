f84c255ff7d5456009383a216361db86123a725f
Sumitbose05 <bosesomen062@gmail.com>
2025-03-20 11:25:45 +0530

    cors updated

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 require("dotenv").config();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://rankcheck.netlify.app"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }
